bddb4fce8725a2984eeb1d5a196866bc23c33b6a
parshin <ilyaiparshin@gmail.com>
2019-11-08 17:17:11 +0500

    Part 4 done

--- catalog/models.py
+++ catalog/models.py
@@ -26,7 +26,9 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    
+
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -34,6 +36,24 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
+class Language(models.Model):
+    """Model represents a language"""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
 
 class BookInstance(models.Model):
