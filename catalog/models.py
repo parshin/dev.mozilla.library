b7365cd841621dc5f01dbec80ae8d60c3a4766b7
parshin <ilyaiparshin@gmail.com>
2019-12-03 18:42:45 +0500

    auth...

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 
 class Genre(models.Model):
@@ -62,6 +64,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                 blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -84,6 +88,12 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
