2857516933ababfaf204bc39de1391540c742413
parshin <ilyaiparshin@gmail.com>
2019-12-09 18:43:23 +0500

    part 8 self work

--- catalog/views.py
+++ catalog/views.py
@@ -71,3 +71,14 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 	def get_queryset(self):
 		return BookInstance.objects.filter(borrower=self.request.user).filter(
 			status__exact='o').order_by('due_back')
+
+
+class LoanedBooksByUsersListView(LoginRequiredMixin, generic.ListView):
+	"""Generic class-based view listing books on loan to current user."""
+	model = BookInstance
+	template_name = 'catalog/bookinstance_list_borrowed.html'
+	paginate_by = 10
+
+	def get_queryset(self):
+		return BookInstance.objects.filter(borrower=self.request.user).filter(
+			status__exact='o').order_by('due_back')
